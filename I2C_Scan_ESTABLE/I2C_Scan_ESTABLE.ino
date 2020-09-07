2ce3626951e3061b4030e2a60b1971f1080f6c1b
Eulogio Lopez <usuariogit@hotmail.com>
2020-09-08 01:31:37 +0200

    Update I2C_Scan_ESTABLE.ino

--- I2C_Scan_ESTABLE/I2C_Scan_ESTABLE.ino
+++ I2C_Scan_ESTABLE/I2C_Scan_ESTABLE.ino
@@ -25,10 +25,9 @@ void setup() {
 Iniciamos el puerto serie para poder mostrar los datos generados
 en consola, e iniciamos tambien el bus I2C.
 */
-  Serial.begin (9600);
+  Serial.begin (115200);
   Wire.begin();
-  Serial.println("Buscando dispositivos en el bus I2C...");
-  Serial.println("");
+  Serial.println(F("Buscando dispositivos en el bus I2C...\n"));
 }
 
 //*******************************************************
@@ -37,41 +36,47 @@ en consola, e iniciamos tambien el bus I2C.
 void loop(){
 
   //Bucle para generar las direciones a escanear
-  for (int i = 1; i < 120; i++)
-  {
+  for (int i = 1; i < 120; i++) {
     Wire.beginTransmission (i);
+    uint8_t respuesta = Wire.endTransmission();
     //Si exite un dispositivo en la direccion llamada, este responderá con un CERO
-    if (Wire.endTransmission () == 0)
-    {
+    if (respuesta == 0) {
       //mostrar datos del dispositivo encontrado
-      Serial.println ("Disposivivo encontrado en la direccion: ");
-      Serial.print ("Decimal, ");
+      Serial.println (F("Disposivivo encontrado en la direccion: "));
+      Serial.print (F("Decimal, "));
       Serial.print (i);
-      Serial.print (" ---- Hexadecimal, (0x"); 
+      Serial.print (F(" ---- Hexadecimal, (0x"));
+      if (i<16) { Serial.print(F("0")); }
       Serial.print (i, HEX);
-      Serial.println (")");
+      Serial.println (F(")"));
       numeroDispositivos++;
-    } 
+    }
+    else if (respuesta == 4) {
+      Serial.print(F("Error en la direccion  0x"));
+      if (i<16) { Serial.print(F("0")); }
+      Serial.println (i, HEX);
+    }
+   
     delay (10); 
   } // Fin del bucle de busqueda
 
   //Mostrar resumen de la busqueda
-  Serial.println ("\n****************************************");
-  Serial.println ("  Busqueda terminada");
-  Serial.println (" --------------------\n");  
-  if (numeroDispositivos == 0){
-    Serial.println (" No se han encontrado dispositivos");
+  Serial.println (F("\n****************************************"));
+  Serial.println (F("  Busqueda terminada"));
+  Serial.println (F(" --------------------\n"));  
+  if (numeroDispositivos == 0) {
+    Serial.println (F(" No se han encontrado dispositivos"));
   }
-  if (numeroDispositivos == 1){
-    Serial.println (" 1 dispositivo encontrado.");
+  if (numeroDispositivos == 1) {
+    Serial.println (F(" 1 dispositivo encontrado."));
   }
-  if (numeroDispositivos > 1){
-    Serial.print (" Encontrados "); 
+  if (numeroDispositivos > 1) {
+    Serial.print (F(" Encontrados ")); 
     Serial.print (numeroDispositivos); 
-    Serial.println (" dispositivos.");
+    Serial.println (F(" dispositivos."));
   }
-  Serial.println ("****************************************");
-  while (true){
+  Serial.println (F("****************************************"));
+  while (true) {
     //'Pausa' generada por bucle infinito
   }
 }
